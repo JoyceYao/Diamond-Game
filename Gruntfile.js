634ff251a5c69abdfe37d3ade4bd8b18643d5a82
JoyceYao <watasi0839@gmail.com>
2015-12-09 21:36:10 -0500

    change css directory, remove non-used images

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,8 +1,109 @@
 module.exports = function(grunt) {
+  // Auto resize (and pad with white color) an image
+  // into multiple sizes for Google&Apple app submission.
+  // modified by JoyceYao --> windows version
+  // using imagemagick v6
+  // for mac version, please see: https://github.com/yoav-zibin/TicTacToe/blob/gh-pages/Gruntfile.js
+  var src_img = "src.png";
+  var src_img_width_to_height_ratio = 1024/1024;
+  var directory = "auto_resize_images";
+  var output_directory = directory + "\\output";
+  var padColor = "FFFFFF"; // white in HEX
+  var desired_sizes = [
+    "1024x1024",
+    "1024x500",
+    "512x512",
+    "50x50",
+    "200x200",
+    "320x50",
+    "android/7inTablet-615x984",
+    "android/10inTablet-656x1048",
+    "android/Phone-656x1054",
+    "ios/iPad-768x1024",
+    "ios/iPadPro-2732x2048",
+    "ios/iphone4-3.5inch-640x960",
+    "ios/iphone5-4inch-640x1096",
+    "ios/iphone6-4.7inch-750x1334",
+    "ios/iphone6plus-5.5inch-1242x2208",
+    "phonegap/icon-29",
+    "phonegap/icon-40",
+    "phonegap/icon-57",
+    "phonegap/icon-58",
+    "phonegap/icon-60",
+    "phonegap/icon-72",
+    "phonegap/icon-76",
+    "phonegap/icon-80",
+    "phonegap/icon-114",
+    "phonegap/icon-120",
+    "phonegap/icon-144",
+    "phonegap/icon-152",
+    "phonegap/icon-180",
+    "phonegap/icon-512",
+    "phonegap/splash-1536x2048",
+    "phonegap/splash-1024x768",
+    "phonegap/splash-1242x2208",
+    "phonegap/splash-2048x1536",
+    "phonegap/splash-2208x1242",
+    "phonegap/splash-320x480",
+    "phonegap/splash-640x1136",
+    "phonegap/splash-640x960",
+    "phonegap/splash-750x1334",
+    "phonegap/splash-768x1024",
+    "phonegap/splash-512x512",
+  ];
+  var commands = [];
+  var commands2 = [];
+  var subdirectories = {};
+  commands.push('rmdir /s/q ' + output_directory);
+  commands.push('mkdir ' + output_directory);
+  for (var i = 0; i < desired_sizes.length; i++) {
+    var desired_size = desired_sizes[i];
+    if (desired_size.split('/').length >= 3) {
+      throw new Error("You can have at most one sub-directory in filename: " + desired_size);
+    }
+    var slashIndex = desired_size.indexOf("/");
+    if (slashIndex !== -1) {
+      var subdirectory = desired_size.substring(0, slashIndex);
+      if (!subdirectories[subdirectory]) {
+        subdirectories[subdirectory] = true;
+        commands.push('mkdir ' + output_directory + '\\' + subdirectory);
+      }
+    }
+    var lastDashIndex = desired_size.lastIndexOf("-");
+    var dimensions = desired_size.substring(lastDashIndex + 1);
+    var width_height = dimensions.split("x");
+    var width = Number(width_height[0]);
+    var height = Number(width_height[width_height.length - 1]);
+    if (dimensions != '' + (width_height.length == 1 ? width : width + "x" + height)) {
+      throw new Error("Illegal dimension size in filename '" + desired_size + "'. You should have a dimension suffix, e.g., 'blabla-512x200'");
+    }
+
+    var fileName = desired_size.replace('/', '\\') + '.png';
+
+    // create resized pictures in temp folders
+    var commandStr = 'convert ' + directory + '\\' + src_img +
+        ' -resize ' + width + 'x' + height +
+        ' -size ' + width + 'x' + height +
+        ' -gravity center' + 			// put the image in the vertical and horizontal center
+        ' -background transparent' + 	// make background transparent
+        ' -extent ' + dimensions +		// force the image to fit the require pixel size
+        ' -quality 75' +   				// compress the image
+        ' ' + output_directory + '\\' + fileName;
+
+    //console.log("make command=" + commandStr);
+    commands.push(commandStr);
+  }
+  var auto_resize_images_command = commands.join(" & ");
+
+
   // Project configuration.
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-
+    shell: {
+      auto_resize_images_on_win: {
+        command: auto_resize_images_command
+      }
+    },
     karma: {
       unit: {
         configFile: 'karma.conf.js',
@@ -52,12 +153,12 @@ module.exports = function(grunt) {
             'http://yoav-zibin.github.io/emulator/dist/turnBasedServices.3.min.js',
             'http://yoav-zibin.github.io/emulator/main.css',
             'dist/everything.min.js',
-            'game.css',
-            'imgs/Diamond_Game_mini.svg',
-            'imgs/HelpSlide1_min.png',
-            'imgs/HelpSlide2_min.png',
-            'imgs/HelpSlide3_min.png',
-            'imgs/HelpSlide4_min.png'
+            'css/game.css',
+            'imgs/Diamond_Game.svg',
+            'imgs/HelpSlide1.png',
+            'imgs/HelpSlide2.png',
+            'imgs/HelpSlide3.png',
+            'imgs/HelpSlide4.png'
           ],
           network: [
             'dist/everything.min.js.map',
@@ -102,6 +203,7 @@ module.exports = function(grunt) {
   // Default task(s).
   grunt.registerTask('default', [
       //'karma',
+      //'shell',
       'concat', 'uglify',
       'processhtml', 'manifest']); //'http-server', 'protractor,
 };
